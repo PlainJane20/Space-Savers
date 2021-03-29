121119fb34184c393956a9c6b324089f50f9b2c0
Svetlana Akimova <lana@Svetlanas-MacBook-Pro.local>
2021-03-29 14:57:29 -0700

    change library that get exif from file. Now we use exifread

--- filesExtraction.py
+++ filesExtraction.py
@@ -1,56 +1,49 @@
-from PIL import Image
-from PIL.ExifTags import GPSTAGS, TAGS
+# from PIL import Image
+# from PIL.ExifTags import GPSTAGS, TAGS
 import json, os
-import io
+import exifread
 
+def getDecimalLatLong(lat, longt):
+    lat_h = lat[0]
+    lat_m = str(lat[1]).split("/")
+    lat_min = 0
+    if len(lat_m) > 1: lat_min = (float(lat_m[0]) / float(lat_m[1])) / 60
+    else: lat_min = float(lat_m[0]) / 60
 
-def getLatLong(path_to_file):
-    latitude = ""
-    longtitude = ""
-    with io.open(path_to_file, 'rb') as file:
-        image = Image.open(file)
-        exifdata = image.getexif()
+    lat_s = str(lat[2]).split("/")
+    lat_sec = 0
+    if len(lat_s) > 1: lat_sec = (float(lat_s[0]) / float(lat_s[1])) / 3600
+    else: lat_sec = float(lat[2]) / 3600
+    latitude = lat_h + lat_min + lat_sec
 
-        for tag_id in exifdata:
-            tag = TAGS.get(tag_id, tag_id)
-            data = exifdata.get(tag_id)     
+    # longt_values = longt.values
+    longt_h = longt[0]
+    longt_m = str(longt[1]).split("/")
+    longt_min = 0
+    if len(longt_m) > 1: longt_min = (float(longt_m[0]) / float(longt_m[1])) / 60
+    else: longt_min = float(longt_m[0]) / 60
 
-            if tag == 'GPSInfo':
-                for key in data.keys():
-                    name = GPSTAGS.get(key, key)
-                
-                    if name == 'GPSLatitudeRef':
-                        lat_ref = data.get(key)
-                    if name == 'GPSLatitude':
-                        lat = data.get(key)
-                        lat = float(lat[0] + (lat[1] / 60) + (lat[2] / 3600))
-                        if lat_ref == 'N': latitude = f'{lat}'
-                        else: latitude = f'-{lat}'
-                        
-                    if name == 'GPSLongitudeRef':
-                        long_ref = data.get(key)
-                    if name == 'GPSLongitude':
-                        longt = data.get(key)
-                        longt = float(longt[0] + (longt[1] / 60) + (longt[2] / 3600))
-                        if long_ref == "E": longtitude = f'{longt}'
-                        else: longtitude = f'-{longt}'
+    longt_s = str(longt[2]).split("/")
+    longt_sec = 0
+    if len(longt_s) > 1: longt_sec = (float(longt_s[0]) / float(longt_s[1])) / 3600
+    else: longt_sec = float(longt[2]) / 3600
+    longtitude = longt_h + longt_min + longt_sec
 
-    return([latitude, longtitude])
-    
+    return [latitude, longtitude]  
     
 def getFileInfo(path_to_folder):
     # get the list of files in given directory
     
     fileInfo = []
+    supported_extensions = ['jpg', 'cr2', 'jpeg']
     
     try: 
         fileList = os.listdir(path_to_folder)
         for file in range(len(fileList)):
             
             split_file_name = os.path.basename(fileList[file]).split('.')
-            print(split_file_name)
-            
-            if split_file_name[-1] == 'jpg':
+
+            if split_file_name[-1].lower() in supported_extensions:
                 file_id = fileList[file]
                 file_path = f"{path_to_folder}/{fileList[file]}"
                 file_size = os.path.getsize(f"{path_to_folder}/{fileList[file]}")
@@ -59,36 +52,48 @@ def getFileInfo(path_to_folder):
                 madeBy = ''
                 model = ''
                 lens = ''
+                lat_ref = ''
+                long_ref = ''
                 coordinates = []
 
-                with io.open(file_path, 'rb') as file:
-                    image = Image.open(file)
-                    exifdata = image.getexif()
-
-                    for tag_id in exifdata:
-                        tag = TAGS.get(tag_id, tag_id)
-                        data = exifdata.get(tag_id)              
-                        if tag == 'DateTimeOriginal': 
-                            date_and_time = data .split(" ")         
+                with open(file_path, 'rb') as file:
+                    tags = exifread.process_file(file)
+                    for tag in tags.keys():
+                        if tag == 'Image Make': madeBy = str(tags[tag])
+                        if tag =='Image Model': model = str(tags[tag])
+                        if tag =='EXIF DateTimeOriginal':
+                            date_and_time = str(tags[tag]).split(' ')
                             date = date_and_time[0]
                             time = date_and_time[1]
-                                           
-                        if tag == "Make": madeBy = data
-                        if tag == 'Model': model = data
-                        if tag == 'LensModel': lens = data
-                        if tag == "GPSInfo": coordinates = getLatLong(file_path)
 
+                        if tag =='MakerNote LensModel': lens = str(tags[tag])
+                        if tag == "GPS GPSLatitudeRef": lat_ref = str(tags[tag])
+
+                        if tag == "GPS GPSLatitude":
+                            lat = tags[tag].values
+                            coordinates.append(lat) 
+
+                        if tag == "GPS GPSLongitudeRef": long_ref = str(tags[tag])
+                        if tag == "GPS GPSLongitude":
+                            longt = tags[tag].values
+                            coordinates.append(longt)
+                        
+                decimal_coordinates = []   
+                if len(coordinates) != 0:
+                    decimal_coordinates = getDecimalLatLong(coordinates[0], coordinates[1])
+                    if lat_ref != 'N': decimal_coordinates[0] = float(f'-{decimal_coordinates[0]}')
+                    if long_ref != 'E': decimal_coordinates[1] = float(f'-{decimal_coordinates[1]}')
 
                 fileInfo.append({"file_id": file_id, "file_path": file_path, "file_bytes_size": file_size
                                 , "Date": date, "Time": time, "lensType": lens
-                                , 'madeBy': madeBy, 'model': model, 'coordinates': coordinates})
+                                , 'madeBy': madeBy, 'model': model, 'coordinates': decimal_coordinates})
         return fileInfo
         
     except FileNotFoundError:
         return "Folder not found"
 
 
-# SHOUKD BE MOVED TO APP>PY
+# SHOUKD BE MOVED TO APP.PY
 def getUserpath(path_to_folder):
     if os.path.exists("filesInfo.json"):
         os.remove("filesInfo.json")
@@ -97,4 +102,4 @@ def getUserpath(path_to_folder):
         json.dump(getFileInfo(path_to_folder), out)
 
 
-getUserpath("/Users/lana/DataClass/SpaceCleaner/Resources/temp_img")
+getUserpath("/Users/lana/Desktop/PhotoForProject/jpg")
