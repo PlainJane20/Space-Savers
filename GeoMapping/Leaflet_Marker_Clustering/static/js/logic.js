2110c117f5ad59101d2074f71ee3c59a48750276
Robert <robertsmith1995@gmail.com>
2021-03-29 23:22:28 -0700

    Updated GeoMapping

--- GeoMapping/Leaflet_Marker_Clustering/static/js/logic.js
+++ GeoMapping/Leaflet_Marker_Clustering/static/js/logic.js
@@ -16,26 +16,25 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 
-// Use this link to get the geojson data.
-//var link = "Analysis_Neighborhoods.geojson";
-//var url = "[https://data.sfgov.org/resource/g8m3-pdis.json]"
-
+// read JSON file
+var url = "static/js/filesInfo.json";
+console.log(url);
 
 d3.json(url, function(response) {
-
-  console.log(response);
+console.log(response);
+  // Create a new marker cluster group
   var markers = L.markerClusterGroup();
   response.forEach(element => {
-    var location = element.location;
-console.log(location);
-    // Check for location property
-    if (location) {
-      console.log("Hey, it works!");
-      // Add a new marker to the cluster group and bind a pop-up
-   
-    markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-    .bindPopup("<h1>" + element.dba_name + "</h1> <hr> <h3>Neighborhood: " + element.neighborhoods_analysis_boundaries + "</h3>")).addTo(myMap) ;
-}
+    var coordinates = element.coordinates;
 
-});
+    // Check for location property
+    if (coordinates.length == 2) {
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([coordinates[0], coordinates[1]])
+      .bindPopup("<h1>" + element.file_id + "</h1> <hr> <h3>Date: " + element.Date + "</h3>" + "</h1> <hr> <h3>Time: " + element.Time + "</h3>"));
+    } 
+
+  });
+  markers.addTo(myMap);
+
 });
