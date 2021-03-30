21ef99d06c81d0a6f02d83781eff6464a5bc438f
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-03-29 19:14:30 -0700

    updated uploadflak.py

--- uploadflask.py
+++ uploadflask.py
@@ -1,17 +1,26 @@
+####################################
+# Flask Setup
+####################################
 import os
 from flask import Flask, request, redirect, url_for, render_template, jsonify, make_response
 from datetime import datetime
-# from app import app 
+# import 
 
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = "static/img/uploads/"
+####################################
+# Flask Routes
+####################################
 
 @app.route("/", methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
-        print(request.files["file"])
+
+        print(request.files['image'])
         if request.files: 
             image = request.files['image']
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"]. image.filename))
+            print(image.filename)
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
             print("image saved")
             return redirect(request.url)
     return render_template("index.html")
