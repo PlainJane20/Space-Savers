3a251cc4868f9d714fa257b340bbb173db57f344
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-03-29 20:52:31 -0700

    flask script updated to upload multiple files

--- uploadflask.py
+++ uploadflask.py
@@ -4,10 +4,14 @@
 import os
 from flask import Flask, request, redirect, url_for, render_template, jsonify, make_response
 from datetime import datetime
+from werkzeug.utils import secure_filename
 # import 
 
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = "static/img/uploads/"
+
+path = os.getcwd()
+
 ####################################
 # Flask Routes
 ####################################
@@ -15,14 +19,20 @@ app.config["IMAGE_UPLOADS"] = "static/img/uploads/"
 @app.route("/", methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
-
-        print(request.files['image'])
-        if request.files: 
-            image = request.files['image']
-            print(image.filename)
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            print("image saved")
+        files = request.files.getlist("image[]")
+        print(files)
+        if request.files:
+            files = request.files.getlist("image[]")
+            for image in files:
+                print(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+                print("image saved")
             return redirect(request.url)
+            # image = request.files('image')
+            # print(image.filename)
+            # image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            # print("image saved")
+            # return redirect(request.url)
     return render_template("index.html")
 
 if __name__ == "__main__":
